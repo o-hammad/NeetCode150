f93b4278246fb8f45163dc3fda48bd4992fc62ac
Omar <ohammad@gwu.edu>
2024-01-12 11:26:27 -0500

    two sum II

---
+++ two_sum_II.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curr_sum = numbers[l] + numbers[r]
+            if curr_sum > target:
+                r -= 1
+            elif curr_sum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
