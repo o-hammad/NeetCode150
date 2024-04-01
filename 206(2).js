1a5607fda5cb44422a4030bb803a1ef8c98cf167
Omar <ohammad@gwmail.gwu.edu>
2024-04-01 17:43:24 -0400

    206(2)

---
+++ 206(2).js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    let prev = null;
+    let curr = head;
+
+    while (curr) {
+        let nextNode = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    return prev;
+};
+
+// use two pointers, prev and curr
+// shift pointers each iteration
+// switch the direction of the links
